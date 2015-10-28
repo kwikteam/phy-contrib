7948d1d90ea68bd7efc64bf7bcb6770fee31193b
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-28 17:28:16 +0100

    Add files

---
+++ Makefile
@@ -0,0 +1,30 @@
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr *.egg-info
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+
+clean: clean-build clean-pyc
+
+lint:
+	flake8 phycontrib
+
+test: lint
+	py.test --cov-report term-missing --cov=phycontrib phycontrib
+
+coverage:
+	coverage --html
+
+apidoc:
+	python tools/api.py
+
+build:
+	python setup.py sdist --formats=zip
+
+upload:
+	python setup.py sdist --formats=zip upload
---
+++ phycontrib/kwik_gui/gui.py
@@ -0,0 +1,30 @@
+
+# import sys
+
+import click
+# import numpy as np
+
+from phy import IPlugin
+# from phy.cluster.manual import ManualClustering
+from phy.gui import GUI, create_app, run_app
+
+
+class KwikGUI(GUI):
+    def __init__(self, path):
+        self.path = path
+        # TODO
+        # model = KwikModel(path)
+        # config = load_master_config()
+        # plugins = config.KwikGUI.plugins
+        # attach_plugins(gui, plugins)
+
+
+class KwikGUIPlugin(IPlugin):
+    def attach_to_cli(self, cli):
+        @cli.command('cluster-manual')
+        @click.argument('path', type=click.Path(exists=True))
+        def cluster_manual(path):
+            create_app()
+            gui = KwikGUI(path)
+            gui.show()
+            run_app()
