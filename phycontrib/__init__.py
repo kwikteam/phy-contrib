b7ce3418ad1041446380e9245cf34d1d5baff6dd
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-03-24 16:21:29 +0100

    Fix missing static files in dist

--- phycontrib/__init__.py
+++ phycontrib/__init__.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 # Default config and state files
 #------------------------------------------------------------------------------
 
-__version__ = '1.0.0'
+__version__ = '1.0.5'
 
 
 def _copy_gui_state(gui_name, module_name):
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa
 
 """Installation script."""
 
@@ -10,7 +11,6 @@
 import os
 import os.path as op
 import re
-from textwrap import dedent
 
 from setuptools import setup
 
@@ -19,8 +19,19 @@ from setuptools import setup
 # Setup
 #------------------------------------------------------------------------------
 
-# Find version number from `__init__.py` without executing it.
+def _package_tree(pkgroot):
+    path = op.dirname(__file__)
+    subdirs = [op.relpath(i[0], path).replace(op.sep, '.')
+               for i in os.walk(op.join(path, pkgroot))
+               if '__init__.py' in i[2]]
+    return subdirs
+
+
 curdir = op.dirname(op.realpath(__file__))
+readme = open(op.join(curdir, 'README.md')).read()
+
+
+# Find version number from `__init__.py` without executing it.
 filename = op.join(curdir, 'phycontrib/__init__.py')
 with open(filename, 'r') as f:
     version = re.search(r"__version__ = '([^']+)'", f.read()).group(1)
@@ -29,22 +40,26 @@ with open(filename, 'r') as f:
 setup(
     name='phycontrib',
     version=version,
-    license='BSD',
-    description='phycontrib',
-    long_description='',
+    license="BSD",
+    description='Spike sorting and ephys data analysis '
+                'for 1000 channels and beyond',
+    long_description=readme,
     author='Kwik Team',
     author_email='cyrille.rossant at gmail.com',
     url='https://phy.cortexlab.net',
-    packages=[
-        'phycontrib',
-        'phycontrib.kwik_gui',
-        'phycontrib.template',
-    ],
+    packages=_package_tree('phycontrib'),
+    package_dir={'phycontrib': 'phycontrib'},
+    package_data={
+        'phycontrib': ['*.npy', '*.gz', '*.txt', '*.json', '*.prb'],
+    },
+    include_package_data=True,
+    keywords='phy,data analysis,electrophysiology,neuroscience',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
+        "Framework :: IPython",
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
