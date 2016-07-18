0e221cd2163931ec386a3b0cd5052eeb6077ec80
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-07-18 13:16:55 +0200

    Do not precache if data is already cached

--- phycontrib/utils/precache.py
+++ phycontrib/utils/precache.py
@@ -1,3 +1,6 @@
+import os
+import os.path as op
+
 from tqdm import tqdm
 
 from phy import IPlugin
@@ -7,6 +10,10 @@ class PrecachePlugin(IPlugin):
     def attach_to_controller(self, controller):
         @controller.connect
         def on_init():
+            # Skip if the cache has already been created.
+            if op.exists(op.join(controller.cache_dir, 'done')):
+                return
+            # Create the cache.
             for clu in tqdm(controller.cluster_ids.tolist(),
                             desc="Precaching data",
                             leave=True,
@@ -14,3 +21,6 @@ class PrecachePlugin(IPlugin):
                 controller.get_features(clu)
                 controller.get_waveforms(clu)
                 controller.get_close_clusters(clu)
+            # Mark the cache as complete.
+            with open(op.join(controller.cache_dir, 'done'), 'w') as f:
+                f.write('')
