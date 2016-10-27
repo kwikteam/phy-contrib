9bb574438253bde9a24574a909ac2bf8ae580ebb
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-10-27 12:41:26 +0200

    Bump version

--- phycontrib/__init__.py
+++ phycontrib/__init__.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 # Default config and state files
 #------------------------------------------------------------------------------
 
-__version__ = '1.0.13'
+__version__ = '1.0.14'
 
 
 def _copy_gui_state(gui_name, module_name):
