e3c72957f6ead9428626e0f109c5d565a1d1713d
Cyrille Rossant <rossant@users.noreply.github.com>
2018-08-15 16:49:12 +0200

    Update saveprompt plugin

--- phycontrib/utils/saveprompt.py
+++ phycontrib/utils/saveprompt.py
@@ -6,32 +6,35 @@
 # Imports
 #------------------------------------------------------------------------------
 
+from functools import partial
+
 from phy.gui.qt import _prompt, _show_box
 
 from phy import IPlugin
+from phy.utils import connect
 
 
 #------------------------------------------------------------------------------
 # Plugin
 #------------------------------------------------------------------------------
 
+def prompt_save(supervisor):
+    # Show save prompt if an action was done.
+    if len(supervisor._global_history) <= 1:
+        return
+    b = _prompt("Do you want to save your modifications "
+                "before quitting?",
+                buttons=['save', 'cancel', 'close'],
+                title='Save')
+    r = _show_box(b)
+    if r == 'save':
+        supervisor.save()
+    elif r == 'cancel':
+        return False
+    elif r == 'close':
+        return
+
+
 class SavePromptPlugin(IPlugin):
     def attach_to_controller(self, controller):
-        @controller.connect
-        def on_create_gui(gui):
-            @gui.connect_
-            def on_close():
-                # Show save prompt if an action was done.
-                if len(controller.supervisor._global_history) <= 1:
-                    return
-                b = _prompt("Do you want to save your modifications "
-                            "before quitting?",
-                            buttons=['save', 'cancel', 'close'],
-                            title='Save')
-                r = _show_box(b)
-                if r == 'save':
-                    controller.supervisor.save()
-                elif r == 'cancel':
-                    return False
-                elif r == 'close':
-                    return
+        connect(partial(prompt_save, controller.supervisor), event='close')
